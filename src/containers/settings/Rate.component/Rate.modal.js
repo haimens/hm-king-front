bbf4a1eefa532a1e6baee17abf4c16c23dd761e2
Jeremy Guan <jeremyguan1@gmail.com>
2019-06-17 17:55:30 -0700

    Update Rate.modal.js

--- src/containers/settings/Rate.component/Rate.modal.js
+++ src/containers/settings/Rate.component/Rate.modal.js
@@ -3,12 +3,6 @@ import { Modal } from "../../../components/shared";
 
 export default class RateModal extends Component {
   state = {
-    img_url: "",
-    showImage: false,
-    showPreview: false,
-    company_name: "",
-    company_address: "",
-    company_title: "",
     fee_rate: ""
   };
 
@@ -30,7 +24,7 @@ export default class RateModal extends Component {
   componentDidMount() {}
 
   render() {
-    const { img_url, showImage, showPreview, company_name, company_address, company_title, fee_rate } = this.state;
+    const { fee_rate } = this.state;
     return (
       <Modal
         title="创建盘口"
@@ -42,21 +36,16 @@ export default class RateModal extends Component {
       >
         <div className="container">
           <form className="p-3" onSubmit={this.handleSubmit}>
-            <div className="form-group">
-              <label htmlFor="company_title">Email</label>
-              <input
-                type="cell"
-                className="form-control"
-                name="company_title"
-                id="company_title"
-                value={company_title}
-                onChange={this.handleInputChange}
-              />
+            <label for="basic-url">Your vanity URL</label>
+            <div className="input-group mb-3">
+              <input type="text" className="form-control" id="basic-url" aria-describedby="basic-addon3" />
+              <div className="input-group-append">
+                <span className="input-group-text">%</span>
+              </div>
             </div>
-
             <div className="form-group text-center p-3">
-              <button className="hm-bg-green btn btn-sm px-4 text-white mr-3">Add</button>
-              <button onClick={this.handleCancel} className="btn btn-sm btn-outline-secondary px-4">
+              <button className="hm-bg-green btn px-4 text-white mr-3">Add</button>
+              <button onClick={this.handleCancel} className="btn btn-outline-secondary px-4">
                 Cancel
               </button>
             </div>
