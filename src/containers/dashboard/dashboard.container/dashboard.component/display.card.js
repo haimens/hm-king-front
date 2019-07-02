1a054394e7f51d23853f67a6870f9595d9d4fbfa
Jeremy Guan <jeremyguan1@gmail.com>
2019-07-02 15:27:57 -0700

    style quicl update

--- src/containers/dashboard/dashboard.container/dashboard.component/display.card.js
+++ src/containers/dashboard/dashboard.container/dashboard.component/display.card.js
@@ -10,19 +10,17 @@ export function DisplayCardItem(props) {
   const { amount, title, icon } = props.data;
   return (
     <main className="bg-white rounded border shadow-sm display-card-container p-3">
-      <section className="container ">
-        <div className="row">
-          <div className="col">
-            <div className="text-secondDary-text-color font-weight-bold">TOTAL COMPANY</div>
-          </div>
-          <div className="col-auto col ">
-            <div className="card-icon-background  d-flex justify-content-center  align-items-center rounded-circle shadow">
-              <img
-                src={icon || `${process.env.PUBLIC_URL}/img/icon_24hr.svg`}
-                alt={icon || `${process.env.PUBLIC_URL}/img/available.svg`}
-                className="card-img"
-              />
-            </div>
+      <section className="row">
+        <div className="col">
+          <div className="text-secondDary-text-color font-weight-bold">TOTAL COMPANY</div>
+        </div>
+        <div className="col-auto col">
+          <div className="card-icon-background  d-flex justify-content-center  align-items-center rounded-circle shadow">
+            <img
+              src={icon || `${process.env.PUBLIC_URL}/img/icon_24hr.svg`}
+              alt={icon || `${process.env.PUBLIC_URL}/img/available.svg`}
+              className="card-img"
+            />
           </div>
         </div>
       </section>
--- src/containers/dashboard/dashboard.container/dashboard.container.js
+++ src/containers/dashboard/dashboard.container/dashboard.container.js
@@ -13,28 +13,30 @@ class Dashboard extends Component {
     return (
       <main>
         <section className="mb-4">
-          <div className="d-flex align-items-center mb-4  text-white">
+          <div className="d-flex align-items-center mb-4 px-2  text-white">
             <i className="fas fa-desktop hm-header-size mr-3" />
             <h4 className="hm-header-size">Dashboard</h4>
           </div>
-          <div className="row">
-            <div className="col-12 col-md-6 h-100">
-              <DisplayCard
-                data={{
-                  amount: 20,
-                  title: "Total Company",
-                  icon: `${process.env.PUBLIC_URL}/img/icon_company.svg`
-                }}
-              />
-            </div>
-            <div className="col-12 col-md-6 h-100">
-              <DisplayCard
-                data={{
-                  amount: 20,
-                  title: "Total Invoice",
-                  icon: `${process.env.PUBLIC_URL}/img/icon_invoice.svg`
-                }}
-              />
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-12 col-md-6 h-100 mb-4">
+                <DisplayCard
+                  data={{
+                    amount: 20,
+                    title: "Total Company",
+                    icon: `${process.env.PUBLIC_URL}/img/icon_company.svg`
+                  }}
+                />
+              </div>
+              <div className="col-12 col-md-6 h-100">
+                <DisplayCard
+                  data={{
+                    amount: 20,
+                    title: "Total Invoice",
+                    icon: `${process.env.PUBLIC_URL}/img/icon_invoice.svg`
+                  }}
+                />
+              </div>
             </div>
           </div>
         </section>
